d9e618660132b79358f56a6982bdadc57bd24632
likelytwitchdollop <cynthiamahofa@yahoo.com>
2024-07-17 15:38:25 +0200

    fix: revert changes for deployment

--- src/config/config.ts
+++ src/config/config.ts
@@ -13,13 +13,14 @@ const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ''
 
 export const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@ac-kwlr9yf-shard-00-00.wpymslr.mongodb.net:27017,ac-kwlr9yf-shard-00-01.wpymslr.mongodb.net:27017,ac-kwlr9yf-shard-00-02.wpymslr.mongodb.net:27017/?ssl=true&replicaSet=atlas-dl9fwt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
 
-/*
-  const SERVER_PORT = process.env.SERVER_PORT
-    ? Number(process.env.SERVER_PORT)
-    : 1337
-*/
+const SERVER_PORT = process.env.SERVER_PORT
+	? Number(process.env.SERVER_PORT)
+	: 1337
 
-const SERVER_PORT = isDevelopment ? 1337 : process.env.PORT || 4000
+// For Render.io.
+/*
+  const SERVER_PORT = isDevelopment ? 1337 : process.env.PORT || 4000
+*/
 
 const config = {
 	db: { uri: MONGO_URI },

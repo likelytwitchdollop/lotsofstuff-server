5de32c5e769e62e29a8a03464e12e4775637ca90
likelytwitchdollop <cynthiamahofa@yahoo.com>
2024-07-17 15:26:17 +0200

    fix: update server port

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,19 +2,24 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const BASE_URL =
-	process.env.NODE === 'development'
-		? 'http://localhost:3000'
-		: 'lotsofstuff.vercel.com'
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+const BASE_URL = isDevelopment
+	? 'http://localhost:3000'
+	: 'lotsofstuff.vercel.com'
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || ''
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ''
 
 export const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@ac-kwlr9yf-shard-00-00.wpymslr.mongodb.net:27017,ac-kwlr9yf-shard-00-01.wpymslr.mongodb.net:27017,ac-kwlr9yf-shard-00-02.wpymslr.mongodb.net:27017/?ssl=true&replicaSet=atlas-dl9fwt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
 
-const SERVER_PORT = process.env.SERVER_PORT
-	? Number(process.env.SERVER_PORT)
-	: 1337
+/*
+  const SERVER_PORT = process.env.SERVER_PORT
+    ? Number(process.env.SERVER_PORT)
+    : 1337
+*/
+
+const SERVER_PORT = isDevelopment ? 1337 : process.env.PORT || 4000
 
 const config = {
 	db: { uri: MONGO_URI },
